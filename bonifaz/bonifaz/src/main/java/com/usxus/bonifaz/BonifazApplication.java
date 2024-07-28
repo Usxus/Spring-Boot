baac558befcf447da0d28355a6ff2dc435ef8181
Luis Bonifaz <110284427+Usxus@users.noreply.github.com>
2024-07-28 00:34:56 -0700

    Base spring-boot 3.3.2

---
+++ bonifaz/bonifaz/src/main/java/com/usxus/bonifaz/BonifazApplication.java
@@ -0,0 +1,13 @@
+package com.usxus.bonifaz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BonifazApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BonifazApplication.class, args);
+	}
+
+}
---
+++ bonifaz/bonifaz/src/test/java/com/usxus/bonifaz/BonifazApplicationTests.java
@@ -0,0 +1,13 @@
+package com.usxus.bonifaz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BonifazApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
